d86612dbbda8cb62dc1409a3261e0590758d7e6c
Mak-xz <rogeriomarks5@gmail.com>
2024-03-22 17:14:29 -0300

    Atualização

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const blogApi = axios.create({
-    baseURL: "https://blog-fake-api.onrender.com/",
+    baseURL: "https://blog-fake-api.onrender.com",
     timeout: 8 * 1000,
 })
 
